b2123bc2f1329b133f84d7bee98532673af34739
Salim Fadhley <sal@stodge.org>
2015-02-19 22:25:28 +0000

    random stuff

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+if [ ! -d venv ]; then
+    ./make_venv.sh
+fi
+
 source venv/bin/activate
 cd $DIR/src
 python setup.py develop
